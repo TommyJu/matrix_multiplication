9a84db6ac84569803f674035ccc79583a14b9d77
Tommy Ju <tommyju25@gmail.com>
2023-12-10 23:42:51 -0800

    Plan program

---
+++ multiply_matrix.py
@@ -0,0 +1,51 @@
+"""
+Tommy Ju
+A01347715
+"""
+
+
+def multiply_matrix(first_matrix: list, second_matrix: list) -> list:
+    pass
+
+
+def first_matrix_input() -> list:
+    """
+    Retrieve a valid matrix from the user
+
+    :precondition: a matrix must be non-empty
+    :precondition: a matrix must not have any missing values
+    :postcondition: retrieves a valid matrix for multiplication
+
+    :return: a list representing the matrix
+    """
+    matrix = []
+
+
+def second_matrix_input(first_matrix: list) -> list:
+    """
+    Retrieve the second valid matrix from the user
+
+    :precondition: a matrix must be non-empty
+    :precondition: a matrix must not have any missing values
+    :precondition: the number of rows of the second matrix must equal the number of columns of the first
+    :postcondition: retrieves a valid matrix for multiplication
+
+    :return: a list representing the matrix
+    """
+    pass
+
+
+def print_matrix(matrix: list):
+    """
+    Prints the matrix
+
+    :param matrix: a list representing a matrix
+    """
+    pass
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
