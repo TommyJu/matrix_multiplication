16f0af82f90a82d750447605cdd8c9e0263710c7
Tommy Ju <tommyju25@gmail.com>
2023-12-11 12:03:09 -0800

    Create matrix multiplication logic and helper functions

--- multiply_matrix.py
+++ multiply_matrix.py
@@ -16,8 +16,57 @@ def multiply_matrix(first_matrix: list, second_matrix: list) -> list:
     :precondition: the number of columns of the first matrix must be equal to the number of rows of the second
     :postcondition: correctly determines the product of the  matrices
     :return: a list representing the solution matrix
+
+    >>> multiply_matrix([[1, 2, 3], [4, 5, 6]], [[1], [1], [2]])
+    [[9], [21]]
     """
-    pass
+
+    list_of_columns = covert_columns_to_rows(second_matrix)
+
+    answer = []
+    for matrix_row in first_matrix:
+        # Multiply the row value with the column value at the corresponding index
+        row_result = []
+        for matrix_column in list_of_columns:
+            row_value = multiply_and_sum_row_and_column(matrix_row, matrix_column)
+            row_result.append(row_value)
+        answer.append(row_result)
+
+    return answer
+
+
+def covert_columns_to_rows(matrix):
+    """
+
+    :param matrix:
+    :return:
+
+    >>> covert_columns_to_rows([[1, 2], [3, 4]])
+    [[1, 3], [2, 4]]
+    """
+    # Convert each column in the second matrix to a row in a list
+    list_of_columns = []
+    second_matrix_row_length = len(matrix[0])
+
+    for column_position in range(second_matrix_row_length):
+        column = [row[column_position] for row in matrix]
+        list_of_columns.append(column)
+
+    return list_of_columns
+
+
+def multiply_and_sum_row_and_column(row, column):
+    """
+
+    :param row:
+    :param column:
+    :return:
+
+    >>> multiply_and_sum_row_and_column([1, 2], [2, 3])
+    8
+    """
+    terms_of_products = [row_value * column_value for row_value, column_value in zip(row, column)]
+    return sum(terms_of_products)
 
 
 def first_matrix_input() -> list:
@@ -37,7 +86,7 @@ def first_matrix_input() -> list:
         print("First Matrix:")
         print(create_matrix_table(matrix))
         row_input = input(f"Input the value(s) of row {row_number} for the first matrix.\n"
-                    f"Type 'next' to continue to the next matrix.\n")
+                          f"Type 'next' to continue to the next matrix.\n").strip()
         # Prevent an empty matrix
         if row_input == "next" and not matrix or row_input == "":
             print(f"You must have a non-empty matrix or row value\n")
@@ -47,7 +96,6 @@ def first_matrix_input() -> list:
         if row_input == "next":
             return matrix
 
-        row_input.strip()
         row_values = row_input.split(" ")
 
         # Convert each row value from string to integer
@@ -89,10 +137,7 @@ def second_matrix_input(first_matrix_column_count: int) -> list:
     while True:
         print("Second Matrix:")
         print(create_matrix_table(matrix))
-        row_input = input(f"Input the value(s) of row {row_number} for the second matrix.\n")
-
-        row_input.strip()
-        row_values = row_input.split(" ")
+        row_values = input(f"Input the value(s) of row {row_number} for the second matrix.\n").strip().split(" ")
 
         # Convert each row value from string to integer
         try:
@@ -119,6 +164,7 @@ def second_matrix_input(first_matrix_column_count: int) -> list:
 
         row_number += 1
 
+
 def create_matrix_table(matrix: list):
     """
     Converts the matrix into an ASCII table
@@ -140,7 +186,7 @@ def main():
     sample_matrix = [[1, 2], [3, 4]]
     sample_table = create_matrix_table(sample_matrix)
     # print(first_matrix_input())
-    print(second_matrix_input(3))
+    print(multiply_matrix([[1, 2, 3], [4, 5, 6]], [[1], [1], [2]]))
 
 
 if __name__ == "__main__":
